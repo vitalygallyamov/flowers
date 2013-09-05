d96efd3efa9cd587a5edaf4ae7bd1c6b3767ba3c
Gallyamov Vitaly <vitgvr@gmail.com>
2013-09-05 12:30:00 +0600

    add custom vars for Robokassa

--- protected/controllers/OrderController.php
+++ protected/controllers/OrderController.php
@@ -13,6 +13,9 @@ class OrderController extends Controller
 			$kassa = new Robokassa('vetalgal89', 'robopass1', 'robopass2', true);
 			$kassa->OutSum = $_GET['OutSum'];
 			$kassa->InvId = $_GET['InvId'];
+
+			$kassa->customVars = array('shp_order_id' => $_GET['shp_order_id']);
+
 			if($kassa->checkHash(strip_tags($_GET['SignatureValue']))) echo "OK";
 			else echo "Что-то пошло не так!";
 		}
@@ -21,13 +24,25 @@ class OrderController extends Controller
 	}
 
 	public function actionSuccess(){
-		print_r($_GET);
+		
 		if(isset($_GET)){
 			$kassa = new Robokassa('vetalgal89', 'robopass1', 'robopass2', true);
 			$kassa->OutSum = $_GET['OutSum'];
 			$kassa->InvId = $_GET['InvId'];
-			if($kassa->checkHash(strip_tags($_GET['SignatureValue']))) echo "OK";
-			else echo "Что-то пошло не так!";
+
+			$kassa->customVars = array('shp_order_id' => $_GET['shp_order_id']);
+
+			if($kassa->checkHash(strip_tags($_GET['SignatureValue']))){
+				$order = Orders::model()->findByPk($_GET['shp_order_id']);
+				$order->status = 2; //Оплачен
+
+				Yii::app()->user->setFlash('order-confirm','Спасибо, за заказ!');
+				$this->redirect(array('index'));
+			}
+			else{
+				Yii::app()->user->setFlash('order-confirm','Ошибка!');
+				$this->redirect(array('index'));
+			}
 		}
 		Yii::app()->end();
 	}
